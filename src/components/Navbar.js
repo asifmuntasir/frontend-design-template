1289cb53cec2b8e23973cf94d4e8c370f39c7dda
asif-96 <shuyeb.dk@gmail.com>
2022-09-16 17:02:17 +0600

    navbar design completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import logo from '../assets/images/Adwords.gif'
 import '../assets/nav.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg m-0 p-0 pt-2 navbar-light bg-light">
             <div className="container-md d-flex justify-content-evenly">
                 <div className="logo">
-                    <img src="../assets/images/favicon-96x96.png" alt="pic" />
+                    <img src={logo} alt="" />
                 </div>
                 <form>
                     <i class="ri-search-line search-icon"></i>
@@ -14,16 +15,26 @@ const Navbar = () => {
                 </form>
                 <ul class="nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Active</a>
+                        <i class="ri-home-8-line pb-3 nav-link fs-5 text-dark nav-icon active"></i>
+                        {/* <a class="nav-link active" aria-current="page" href="#">Active</a> */}
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <i class="ri-team-line nav-link pb-3 fs-5 text-dark nav-icon"></i>
+                        {/* <a class="nav-link" href="https://asifmuntasir.github.io/">Link</a> */}
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <i class="ri-notification-3-line nav-link pb-3 fs-5 text-dark nav-icon"></i>
+                        {/* <a class="nav-link" href="https://github.com/asifmuntasir/">Link</a> */}
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <i class="ri-chat-voice-line nav-link pb-3 fs-5 text-dark nav-icon"></i>
+                        {/* <a class="nav-link" href="https://github.com/asifmuntasir/">Link</a> */}
+                    </li>
+                    <li class="nav-item mx-2 d-flex justify-content-center align-items-center">
+                        <i class="ri-user-fill nav-link me-2 fs-5 text-dark user-icon-border"></i>
+                        <span className='me-1'>Asif Muntasir Shauib</span>
+                        <i class="ri-arrow-down-s-line">
+                        </i>
                     </li>
                 </ul>
             </div>
