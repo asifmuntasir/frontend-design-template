9c9d47972ba9a15f37075435f93fd84f7647de5f
asif-96 <shuyeb.dk@gmail.com>
2022-09-17 22:07:49 +0600

    left side layout aside completed

--- src/components/LeftContent.js
+++ src/components/LeftContent.js
@@ -1,31 +1,51 @@
 import React from 'react';
+import GooglePlayIcon from '../assets/images/google-play.png';
+import AppleIcon from '../assets/images/ios.png';
+import UserIcon from '../assets/images/man.png';
 
 const LeftContent = () => {
     return (
-        <div className='my-2 p-3 bg-white rounded'>
-            <h5 className='fw-bold'>Explore</h5>
-            <ul className='p-0 my-3 list-unstyled'>
-                <li className='my-2'>
-                    <i class="ri-save-line fs-4 me-3"></i>
-                    <span>Saved Posts</span>
-                </li>
-                <li className='my-2'>
-                    <i class="ri-user-3-line fs-4 me-3"></i>
-                    <span>People</span>
-                </li>
-                <li className='my-2'>
-                    <i class="ri-group-line fs-4 me-3"></i>
-                    <span>Groups</span>
-                </li>
-                <li className='my-2'>
-                    <i class="ri-pages-line fs-4 me-3"></i>
-                    <span>Pages</span>
-                </li>
-                <li className='my-2'>
-                    <i class="ri-calendar-line fs-4 me-3"></i>
-                    <span>Events</span>
-                </li>
-            </ul>
+        <div>
+            <div className='my-3 p-3 bg-white rounded'>
+                <h5 className='fw-bold'>Explore</h5>
+                <ul className='p-0 my-3 list-unstyled'>
+                    <li className='my-2 d-flex align-items-center'>
+                        <i class="ri-save-line fs-4 me-3"></i>
+                        <p className='mb-0'>Save</p>
+                    </li>
+                    <li className='my-2 d-flex align-items-center'>
+                        <i class="ri-user-3-line fs-4 me-3"></i>
+                        <p className='mb-0'>People</p>
+                    </li>
+                    <li className='my-2 d-flex align-items-center'>
+                        <i class="ri-group-line fs-4 me-3"></i>
+                        <p className='mb-0'>Groups</p>
+                    </li>
+                    <li className='my-2 d-flex align-items-center'>
+                        <i class="ri-pages-line fs-4 me-3"></i>
+                        <p className='mb-0'>Pages</p>
+                    </li>
+                    <li className='my-2 d-flex align-items-center'>
+                        <i class="ri-calendar-line fs-4 me-3"></i>
+                        <p className='mb-0'>Events</p>
+                    </li>
+                </ul>
+            </div>
+            <div className="bg-white rounded p-3">
+                <h5 className='fw-bold'>Download App</h5>
+                <div className='d-flex app-icon'>
+                    <img src={AppleIcon} alt="" />
+                    <img src={GooglePlayIcon} alt="" />
+                </div>
+            </div>
+            <div className="my-3 bg-white rounded p-3">
+                <h5 className='fw-bold'>Suggested People</h5>
+                <div className='d-flex align-items-center mt-4'>
+                    <img className='w-25 me-2' src={UserIcon} alt="" />
+                    <p className='me-4 mt-3'>Mahin Qureshi</p>
+                    <input className='btn border cursor-pointer' type="button" value='Add +' />
+                </div>
+            </div>
         </div>
     );
 };
